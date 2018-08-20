0d6b43862188d1799dd01c4e70a40b6d696bc61d
liuguojin <01551462@yto.net.cn>
2018-08-20 17:03:22 +0800

    uwsgi的两种配置与环境搭建

--- simple2_flask/instance/config.py
+++ simple2_flask/instance/config.py
@@ -8,7 +8,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     # flask config
     SECRET_KEY = 'df6ebcb8-93c4-11e7-a2ab-9801a7aef71d'
-    # PORT = 3000 HOST='0.0.0.0'可以在config中应该这样配置,但这是一个坑 http://flask123.sinaapp.com/article/44/ 不是很懂!
+    # PORT = 3000 HOST='0.0.0.0'没法在config中这样配置,但这是一个坑 http://flask123.sinaapp.com/article/44/ 不是很懂!
     # SERVER_NAME = '0.0.0.0:3000' # 大坑，暂时避免配置它
 
     # DB Config
